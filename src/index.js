3cc7db61df08b8a586f1a5c612d86baa0c2bec58
soulle <saulekazanbaeva3110@gmail.com>
2019-04-02 12:14:18 +0300

    add validation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import isURL from 'validator/lib/isURL';
+import { watch } from 'melanke-watchjs';
+
+const app = () => {
+  const state = {
+    form: {
+      valid: true,
+    },
+  };
+  const input = document.getElementById('addAddress');
+
+  input.addEventListener('keyup', () => {
+    if (input.value === '') {
+      state.form.valid = true;
+    }
+    state.form.valid = isURL(input.value);
+  });
+
+  watch(state, 'form', () => {
+    if (state.form.valid === false) {
+      input.style.border = 'thick solid red';
+    } else {
+      input.style.border = null;
+    }
+  });
+};
+
+app();
