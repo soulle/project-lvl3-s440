ac44560c3c9b7d8e6550759a6488ea19bb9ec236
soulle <saulekazanbaeva3110@gmail.com>
2019-04-06 11:18:58 +0300

    fix validation

--- src/application.js
+++ src/application.js
@@ -2,19 +2,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import isURL from 'validator/lib/isURL';
 import { watch } from 'melanke-watchjs';
-import axios from 'axios';
+// import axios from 'axios';
 import $ from 'jquery';
-import parseData from './parser';
+// import parseData from './parser';
 import { addArticlesPart, addFeedsPart } from './renderers';
 
 const app = () => {
   const state = {
-    form: {
-      valid: true,
-      submit: 'disabled',
-      request: 'not sent',
-    },
-    spinner: 'off',
+    formStatus: 'empty',
     currentURL: null,
     feedsURL: [],
     feeds: [],
@@ -25,76 +20,82 @@ const app = () => {
   const input = document.getElementById('addAddress');
   const submit = document.querySelector('button');
   const spinner = document.querySelector('.spinner-border');
-
+  const feedback = document.querySelector('.invalid-feedback');
   const isValid = value => isURL(value) && !state.feedsURL.includes(value);
 
-  input.addEventListener('input', () => {
-    if (input.value.length === 0) {
-      state.form.valid = true;
-      state.form.submit = 'disabled';
-    } else {
-      state.form.valid = isValid(input.value);
-      state.currentURL = input.value;
-      state.form.submit = state.form.valid ? 'enabled' : 'disabled';
-    }
+  const formStatuses = {
+    empty: () => {
+      input.value = '';
+      submit.disabled = true;
+      spinner.hidden = true;
+      input.classList.remove('is-valid', 'is-invalid');
+    },
+    valid: () => {
+      submit.disabled = false;
+      spinner.hidden = true;
+      input.classList.remove('is-invalid');
+      input.classList.add('is-valid');
+    },
+    invalid: () => {
+      submit.disabled = true;
+      spinner.hidden = true;
+      input.classList.remove('is-valid');
+      input.classList.add('is-invalid');
+      feedback.textContent = 'Input is invalid';
+    },
+    error: () => {
+      submit.disabled = true;
+      spinner.hidden = true;
+      input.classList.remove('is-valid');
+      input.classList.add('is-invalid');
+      feedback.textContent = 'Error';
+    },
+    loading: () => {
+      submit.disabled = true;
+      spinner.hidden = false;
+    },
+  };
+
+  watch(state, 'formStatus', () => {
+    console.log('!', state.formStatus);
+    formStatuses[state.formStatus]();
   });
 
-  const addData = () => {
-    const cors = 'https://cors-anywhere.herokuapp.com/';
-    const url = new URL(`${cors}${state.currentURL}`);
-    axios.get(url)
-      .then((response) => {
-        const { feedTitle, feedDescription, articles } = parseData(response.data);
-        state.feeds = [feedTitle, feedDescription];
-        state.articles = [...articles];
-      });
-  };
+  input.addEventListener('input', () => {
+    if (input.value === '') {
+      state.formStatus = 'empty';
+    } else {
+      state.formStatus = isValid(input.value) ? 'valid' : 'invalid';
+      console.log('state.formStatus', state.formStatus);
+      state.currentURL = input.value;
+    }
+  });
 
   submit.addEventListener('click', () => {
-    state.spinner = 'on';
     state.feedsURL.push(state.currentURL);
-    state.form.submit = 'disabled';
-    state.form.request = 'sent';
-    addData();
+    state.formStatus = 'loading';
   });
 
-  watch(state, 'spinner', () => {
-    spinner.hidden = state.spinner === 'off';
-  });
-
-  watch(state.form, 'request', () => {
-    if (state.form.request === 'sent') {
-      input.value = '';
-    }
-  });
-
-  const makeInitial = () => {
-    state.form.request = 'not sent';
-    state.spinner = 'off';
-    state.currentURL = null;
-  };
+  // const addData = () => {
+  //  const cors = 'https://cors-anywhere.herokuapp.com/';
+  //  const url = new URL(`${cors}${state.currentURL}`);
+  //  axios.get(url)
+  //    .then((response) => {
+  //      const { feedTitle, feedDescription, articles } = parseData(response.data);
+  //      state.feeds = [feedTitle, feedDescription];
+  //      state.articles = [...articles];
+  //    });
+  // };
 
   watch(state, 'feeds', () => {
     addArticlesPart(state.articles);
     addFeedsPart(state.feeds);
-    makeInitial();
-  });
-
-  watch(state, 'form', () => {
-    submit.disabled = state.form.submit === 'disabled';
-    if (state.form.valid === false) {
-      input.style.border = 'thick solid red';
-    } else {
-      input.style.border = null;
-    }
   });
 
   $('#exampleModal').on('show.bs.modal', function f(event) {
     const button = $(event.relatedTarget); // Button that triggered the modal
     const recipient = button.data('description'); // Extract info from data-* attributes
     console.log('recipient', recipient);
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     const modal = $(this);
     modal.find('.modal-title').text('Description');
     modal.find('.modal-body').html(`${recipient}`);
