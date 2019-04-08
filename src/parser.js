222eb53ee8fcfc821698c932a7fdee29a7a3ae0a
soulle <saulekazanbaeva3110@gmail.com>
2019-04-08 20:31:59 +0300

    fix parser

--- src/parser.js
+++ src/parser.js
@@ -2,6 +2,7 @@ const parser = new DOMParser();
 
 export default (data) => {
   const dom = parser.parseFromString(data, 'application/xml');
+  console.log('dom', dom);
 
   if (!dom.querySelector('channel')) {
     throw new Error('There is no RSS in this source');
@@ -13,7 +14,8 @@ export default (data) => {
   const articles = [...dom.querySelectorAll('item')].map((item) => {
     const articleLink = item.querySelector('link').innerHTML;
     const articleTitle = item.querySelector('title').textContent;
-    const articleDescription = item.querySelector('description').textContent;
+    const descriptionPart = item.querySelector('description');
+    const articleDescription = descriptionPart === null ? null : descriptionPart.textContent;
     return { articleLink, articleTitle, articleDescription };
   });
   return { feedTitle, feedDescription, articles };
